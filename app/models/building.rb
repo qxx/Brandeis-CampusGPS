bb347d552fb2dd017367ca5753656c123d1ee004
Xi Qian <charlesqx@gmail.com>
2015-03-03 13:27:59 -0500

    validates building and add tests

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,4 @@
 class Building < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
   has_many :entrances
 end
--- test/models/building_test.rb
+++ test/models/building_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class BuildingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  before do
+    @building = buildings(:one)
+  end
+
+  it "validates a new building" do
+    b = Building.create(name: "name")
+    b.valid?.must_equal true
+  end
+
+  it "cannot save a building without name" do
+    b = Building.create(description: "building")
+    b.valid?.must_equal false
+  end
+
+  it "cannot save a building with a same name" do
+    b = Building.create(name: @building.name)
+    b.valid?.must_equal false
+  end
+
 end
