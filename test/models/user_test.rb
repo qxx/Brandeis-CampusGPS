a38d26cece4fd1c9b04bbc00abd39df0b3abfd1c
ginnyzou2015 <ginnyzou2015@gmail.com>
2015-03-03 14:04:12 -0500

    add test for last admin

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  it "has two users in database" do
+  	User.count.must_equal 2
+  end
+
+  it "delete last user in database" do
+  	User.where(name: "susannah").destroy_all
+    User.count.must_equal 1
+    proc {User.destroy_all}.must_raise RuntimeError
+  end
+
 end
