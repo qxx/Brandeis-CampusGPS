94f0fc14814f9a03c9351584ebd3783971619b35
Wan Zhou <wzhou@brandeis.edu>
2015-03-03 22:03:08 -0500

    Add Path model test

--- app/models/path.rb
+++ app/models/path.rb
@@ -1,2 +1,4 @@
 class Path < ActiveRecord::Base
+  validates :description, presence: true
+  validates :distance, allow_blank: true, numericality: {greater_than_or_equal_to: 0}
 end
--- test/models/path_test.rb
+++ test/models/path_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PathTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :paths
+
+  it "can create a path with a description" do
+    path = Path.new(description: paths(:one).description)
+    path.valid?.must_equal true
+  end
+
+  it "cannot create a path without a description" do
+    path = Path.new
+    path.valid?.must_equal false
+  end
+
+  it "cannot create a path with non-positive distance" do
+    path = Path.new(description: paths(:one).description, distance: -1)
+    path.valid?.must_equal false
+  end
 end
