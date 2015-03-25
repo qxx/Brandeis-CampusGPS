e89b84b86a09f51107f01b3d8a6775b24640fa47
Xi Qian <charlesqx@gmail.com>
2015-03-25 14:18:58 -0400

    add SSC floorplans

--- db/seed_data/paths.rb
+++ db/seed_data/paths.rb
@@ -10,54 +10,6 @@ PATHS = [
   start_location_id: PathSeeds.l('Heller_e1').id,
   end_location_id: PathSeeds.l('Volen_e1').id,
   direction: 0
-}, {
-  description: 'Go back to Heller',
-  distance: 1,
-  start_location_id: PathSeeds.l('Volen_e1').id,
-  end_location_id: PathSeeds.l('Heller_e1').id,
-  direction: 180
-}, {
-  description: 'SSC to Mandel',
-  distance: 2,
-  start_location_id: PathSeeds.l('SSC_e1').id,
-  end_location_id: PathSeeds.l('Mandel_e1').id,
-  direction: 180
-}, {
-  description: 'Mandel to SSC',
-  distance: 2,
-  start_location_id: PathSeeds.l('Mandel_e1').id,
-  end_location_id: PathSeeds.l('SSC_e1').id,
-  direction: 180
-}, {
-  description: 'Volen to Mandel',
-  distance: 3,
-  start_location_id: PathSeeds.l('Volen_e1').id,
-  end_location_id: PathSeeds.l('Mandel_e1').id,
-  direction: 0
-}, {
-  description: 'Mandel to Volen',
-  distance: 3,
-  start_location_id: PathSeeds.l('Mandel_e1').id,
-  end_location_id: PathSeeds.l('Volen_e1').id,
-  direction: 180
-}, {
-  description: 'Volen to SSC',
-  distance: 4,
-  start_location_id: PathSeeds.l('Volen_e1').id,
-  end_location_id: PathSeeds.l('SSC_e1').id,
-  direction: 0
-},  {
-  description: 'Heller to Mandel',
-  distance: 4,
-  start_location_id: PathSeeds.l('Heller_e1').id,
-  end_location_id: PathSeeds.l('Mandel_e1').id,
-  direction: 0
-}, {
-  description: 'Mandel to Heller',
-  distance: 5,
-  start_location_id: PathSeeds.l('Mandel_e1').id,
-  end_location_id: PathSeeds.l('Heller_e1').id,
-  direction: 180
 }, 
 # Real paths
 {
